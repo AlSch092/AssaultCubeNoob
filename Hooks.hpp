13ad14284fd09ed46cfc921d9bc102520b1ba3be
Alex S. <94417808+AlSch092@users.noreply.github.com>
2024-10-31 17:45:04 -0400

    removed accidental extra routines from reusing this file from a different project

--- Hooks.hpp
+++ Hooks.hpp
@@ -9,13 +9,3 @@ struct HOOK {
 };
 
 VOID WriteHookBytes(HOOK hkHook, VOID* lpvHook, BOOL bEnable);
-
-// ---Game hooks for hacking---
-CONST HOOK hkSpeedHack = { dwSpeedHack, dwSpeedHack + 8, "\x89\x44\x24\x14\xDB\x44\x24\x14", 8 };
-VOID MovespeedHook();
-
-CONST HOOK hkCPUReducer = { dwCPUReducer, dwCPUReducer + 5, "\x83\xEC\x18\x53\x55", 5 };
-VOID CPUReducerHook();
-
-CONST HOOK hkVillageSendPacket = { dwSendVillagePacket, dwSendVillagePacket + 5, "\xE8\x91\x38\x00\x00", 5 };
-VOID SendHook();
